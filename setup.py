11d19fc8facbc5bfe492af7fd41cdebdf1483fa4
tymofiishevchenko <tymofii.shevchenko.1905@gmail.com>
2020-05-04 13:47:42 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='bnw',
+    packages=['bnw'],
+    version='1.0',
+    license='MIT',
+    description='Free and easy to use Python library to add some basic effects to images',
+    author='tymofii shevchenko',
+    author_email='tymofii.shevchenko.1905@gmail.com',
+    url='https://github.com/tymofiishevchenko/bnw',
+    download_url='https://github.com/tymofiishevchenko/bnw/archive/1.0.tar.gz',
+    keywords=['image-proccesing'],
+    install_requires=['pillow'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
